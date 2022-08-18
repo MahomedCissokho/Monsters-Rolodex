a32d70d00368279affb2ba793fc912b3f9c28837
Mahomed CISSOKHO <mahomedcissokho@esp.sn>
2022-08-18 13:09:51 +0000

    Add files via upload

---
+++ src/search-box/search-box.component.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import './search-box.style.css';
+
+class SearchBox extends Component {
+    render() {
+        const { onSearchChange, placeholder, className } = this.props;
+        return(
+          <input className={`search-box ${className}`} type="search" placeholder={placeholder} onChange = { onSearchChange}/>
+        )
+    }
+}
+
+
+export default SearchBox;
